53aff651cac18580876541a741ade79c73126847
Parker Anderson <parker.anderson0810@outlook.com>
2021-06-22 15:47:59 -0400

    Initial commit

---
+++ src/main/java/com/mygroup/UserCRUDWebApp/UserCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.UserCRUDWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mygroup/UserCRUDWebApp/UserCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygroup.UserCRUDWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
